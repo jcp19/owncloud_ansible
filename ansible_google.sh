4e5dbf561c6c675dd55abe3de30815b68e037c6d
luciano96 <luckapl96@gmail.com>
2018-01-04 18:39:33 +0000

    New hosts

--- ansible_google.sh
+++ ansible_google.sh
@@ -1,6 +1,6 @@
-grep -o "^\([0-9]*\.\)\{3\}[0-9]*" hosts_google | while read -r line ; do 
-    ssh-keygen -f "/home/luciano/.ssh/known_hosts" -R $line
-    ssh-keyscan -H $line  >> ~/.ssh/known_hosts
-done
+# grep -o "^\([0-9]*\.\)\{3\}[0-9]*" hosts_google | while read -r line ; do 
+#     ssh-keygen -f "/home/luciano/.ssh/known_hosts" -R $line
+#     ssh-keyscan -H $line  >> ~/.ssh/known_hosts
+# done
 
 ansible-playbook playbook.yml -i hosts_google  -b
